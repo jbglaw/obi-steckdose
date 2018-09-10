6ffc34344d72f08bbe3e979c5b126f4b455b7839
Jan-Benedict Glaw <jbglaw@getslash.de>
2018-09-10 15:31:31 +0200

    Common logging also to syslog

--- obi-common.h
+++ obi-common.h
@@ -3,16 +3,6 @@
 
 #include "Syslog.h"
 
-#if 1
-#  define obi_print(...)	do { Serial.print(__VA_ARGS__); Serial.flush (); } while (0)
-#  define obi_printf(...)	do { Serial.printf(__VA_ARGS__); Serial.flush (); } while (0)
-#  define obi_println(...)	do { Serial.println(__VA_ARGS__); Serial.flush (); } while (0)
-#else
-#  define obi_print(...)
-#  define obi_printf(...)
-#  define obi_println(...)
-#endif
-
 #define ARRAY_SIZE(x)	(sizeof(x)/sizeof(x[0]))
 
 extern void relay_set (bool on_p);
@@ -34,5 +24,15 @@ extern Syslog syslog;
 extern bool have_syslog_p;
 extern const char obi_git_commit[];
 extern const char obi_build_timestamp[];
+
+#if 1
+#  define obi_print(...)	do { Serial.print (__VA_ARGS__);   Serial.flush (); if (have_syslog_p) syslog.logf ("%s\n", __VA_ARGS__); } while (0)
+#  define obi_printf(...)	do { Serial.printf (__VA_ARGS__);  Serial.flush (); if (have_syslog_p) syslog.logf (__VA_ARGS__);         } while (0)
+#  define obi_println(...)	do { Serial.println (__VA_ARGS__); Serial.flush (); if (have_syslog_p) syslog.logf ("%s\n", __VA_ARGS__); } while (0)
+#else
+#  define obi_print(...)
+#  define obi_printf(...)
+#  define obi_println(...)
+#endif
 
 #endif /* OBI_COMMON_H  */
